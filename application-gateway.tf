b7df3d563ff8bf77c6152287e40cf1318bba2052
HamzaAlhashmi1 <alhashmihamza@hotmail.com>
2025-05-06 03:54:36 +0300

    Create application-gateway.tf

---
+++ application-gateway.tf
@@ -0,0 +1,112 @@
+# -----------------------------------
+# Public IP for Application Gateway
+# -----------------------------------
+resource "azurerm_public_ip" "web_ag_publicip" {
+  name                = "${local.resource_name_prefix}-web-ag-publicip-${random_string.myrandom.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# -----------------------------------
+# Locals: Reusable Naming Conventions
+# -----------------------------------
+locals {
+  # Generic
+  frontend_port_name              = "${azurerm_virtual_network.vnet.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.vnet.name}-feip"
+  listener_name                  = "${azurerm_virtual_network.vnet.name}-httplstn"
+  request_routing_rule1_name     = "${azurerm_virtual_network.vnet.name}-rrt-1"
+
+  # App1-specific
+  backend_address_pool_name_app1 = "${azurerm_virtual_network.vnet.name}-beap-app1"
+  http_setting_name_app1         = "${azurerm_virtual_network.vnet.name}-be-hstst-app1"
+  probe_name_app1                = "${azurerm_virtual_network.vnet.name}-be-probe-app1"
+}
+
+# -----------------------------------
+# Azure Application Gateway
+# -----------------------------------
+resource "azurerm_application_gateway" "web_ag" {
+  name                = "${local.resource_name_prefix}-web-ag-${random_string.myrandom.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku {
+    name = "Standard_v2"
+    tier = "Standard_v2"
+  }
+
+  autoscale_configuration {
+    min_capacity = 0
+    max_capacity = 10
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.agsubnet.id
+  }
+
+  # -------------------------
+  # Frontend Configurations
+  # -------------------------
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.web_ag_publicip.id
+  }
+
+  # -------------------------
+  # Listener Configuration
+  # -------------------------
+  http_listener {
+    name                          = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name            = local.frontend_port_name
+    protocol                      = "Http"
+  }
+
+  # -------------------------
+  # Backend Configs - App1
+  # -------------------------
+  backend_address_pool {
+    name = local.backend_address_pool_name_app1
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name_app1
+    cookie_based_affinity = "Disabled"
+    port                  = 8501
+    protocol              = "Http"
+    request_timeout       = 60
+    probe_name            = local.probe_name_app1
+  }
+
+  probe {
+    name                = local.probe_name_app1
+    host                = "127.0.0.1"
+    interval            = 30
+    timeout             = 30
+    unhealthy_threshold = 3
+    protocol            = "Http"
+    port                = 8501
+    path                = "/"
+  }
+
+  # -------------------------
+  # Routing Rule
+  # -------------------------
+  request_routing_rule {
+    name                       = local.request_routing_rule1_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name_app1
+    backend_http_settings_name = local.http_setting_name_app1
+  }
+}
