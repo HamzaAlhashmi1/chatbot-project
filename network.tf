9b495f6c16cc186f372f93d7dd227291391012a9
HamzaAlhashmi1 <alhashmihamza@hotmail.com>
2025-05-11 14:18:22 +0300

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,135 @@
+# -----------------------------------
+# Locals & Tags
+# -----------------------------------
+locals {
+  environment          = var.environment
+  resource_name_prefix = var.environment
+
+  common_tags = {
+    environment = local.environment
+  }
+
+  # Inbound Ports for Web Subnet (e.g. SSH, ChromaDB)
+  web_inbound_ports_map = {
+    "120" = "22"     # SSH
+    "130" = "8000"   # Chroma DB
+  }
+
+  # Inbound Ports for App Gateway Subnet
+  ag_inbound_ports_map = {
+    "100" = "80"               # HTTP
+    "110" = "443"              # HTTPS
+    "130" = "65200-65535"      # App Gateway Backend Pool Ports
+  }
+}
+
+# -----------------------------------
+# Random String for Uniqueness
+# -----------------------------------
+resource "random_string" "myrandom" {
+  length  = 3
+  upper   = false
+  special = false
+}
+
+# -----------------------------------
+# Resource Group
+# -----------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.resource_name_prefix}-${var.resource_group_name}-${random_string.myrandom.id}"
+  location = var.resource_group_location
+  tags     = local.common_tags
+}
+
+# -----------------------------------
+# Virtual Network
+# -----------------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${local.resource_name_prefix}-${var.vnet_name}-${random_string.myrandom.id}"
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.common_tags
+}
+
+# -----------------------------------
+# Web Subnet
+# -----------------------------------
+resource "azurerm_subnet" "websubnet" {
+  name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.web_subnet_address
+}
+
+# -----------------------------------
+# NSG for Web Subnet
+# -----------------------------------
+resource "azurerm_network_security_group" "web_subnet_nsg" {
+  name                = "${azurerm_subnet.websubnet.name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Inbound NSG Rules for Web Subnet
+resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
+  for_each                    = local.web_inbound_ports_map
+  name                        = "Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name
+}
+
+# Associate Web NSG with Web Subnet
+resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_associate" {
+  depends_on                = [azurerm_network_security_rule.web_nsg_rule_inbound]
+  subnet_id                 = azurerm_subnet.websubnet.id
+  network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
+}
+
+# -----------------------------------
+# App Gateway Subnet
+# -----------------------------------
+resource "azurerm_subnet" "agsubnet" {
+  name                 = "${azurerm_virtual_network.vnet.name}-${var.ag_subnet_name}-${random_string.myrandom.id}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.ag_subnet_address
+}
+
+# NSG for App Gateway Subnet
+resource "azurerm_network_security_group" "ag_subnet_nsg" {
+  name                = "${azurerm_subnet.agsubnet.name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Inbound NSG Rules for App Gateway Subnet
+resource "azurerm_network_security_rule" "ag_nsg_rule_inbound" {
+  for_each                    = local.ag_inbound_ports_map
+  name                        = "Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.ag_subnet_nsg.name
+}
+
+# Associate App Gateway NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "ag_subnet_nsg_associate" {
+  depends_on                = [azurerm_network_security_rule.ag_nsg_rule_inbound]
+  subnet_id                 = azurerm_subnet.agsubnet.id
+  network_security_group_id = azurerm_network_security_group.ag_subnet_nsg.id
+}
